c45016a14a6c2ac7a2b07f9520f71be7dfe1f068
ZuguangTong <810732627@qq.com>
2022-05-14 23:18:45 +1000

    set IP

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -9,5 +9,5 @@ export const GetSports = () => {
     return request.get('/sports').then((res) => res.data)
 }
 export const GetSuburbs = () => {
-    return request.get('http://localhost:3000/city_suburb_map.json')
+    return request.get(`${process.env.REACT_APP_WEB_SERVER}/city_suburb_map.json`)
 }
